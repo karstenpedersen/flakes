98e709ce8f426dfb3cf4e955e91e59bb9e3ccd54
Karsten Pedersen <karstenfp.all@gmail.com>
2024-02-10 11:20:46 +0100

    Start on Java flake

---
+++ java/default.nix
@@ -0,0 +1,30 @@
+{
+  description = "Java development environment";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      javaVersion = 21;
+      overlays = [
+        (final: prev: rec {
+          jdk = prev."jdk${toString javaVersion}";
+          gradle = prev.gradle.override { java = jdk; };
+          maven = prev.maven.override { inherit jdk; };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system; };
+    in
+    rec {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gnumake
+          jdk
+          gradle
+          maven
+        ];
+      };
+    }
+  );
+}
